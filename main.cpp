3b317997ee764abffafada33d14ac89024ad8786
Rösetta Josephine Kennedy <rosettajosephinekennedy@gmail.com>
2025-09-29 14:39:46 +0100

    Normalised ball movement

--- main.cpp
+++ main.cpp
@@ -1,27 +1,47 @@
 #include <iostream>
-#include "raylib.h"
+#include <raylib.h>
+#include "raymath.h"
+
+const int screenWidth = 960;
+const int screenHeight = 600;
 
 class Ball {
     public:
-    float x = 20, y = 30;
+    Vector2 position = Vector2{50, 50}, direction;
+    float speed = 10;
+    int radius = 40;
 
     void Draw() {
-        DrawCircle(x, y, 10, BLUE);
+        DrawCircle(position.x, position.y, radius, BLUE);
+    }
+
+    void Update() {
+
+        // Direction Movement
+        if (IsKeyDown(KEY_W)) { direction.y = -1; }
+        else if (IsKeyDown(KEY_S)) { direction.y = 1; }
+        else { direction.y = 0; }
+        if (IsKeyDown(KEY_D)) { direction.x = 1; }
+        else if (IsKeyDown(KEY_A)) { direction.x = -1; }
+        else { direction.x = 0; }
+
+        // Applies the normalised movement.
+        position += Vector2Normalize(direction) * speed;
     }
 };
 
 Ball ball;
 
 int main() {
-    const int screenWidth = 960;
-    const int screenHeight = 600;
 
     InitWindow(screenWidth, screenHeight, "Raylib Starter Template");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
+        ball.Update();
+
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
         ball.Draw();
         EndDrawing();
     }
